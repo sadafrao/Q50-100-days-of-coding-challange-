d8f31d0cc3a6b7f4cc5d9672b3cb159dc042c2a7
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 20:30:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+//Q NO 50 using template litrals to define a multiple lines
+var mydailyroutine = "my daily routine is :\n1.wake up early and go to job.\n2. start coding .\n3.make some videos for my yt channel.";
+// now console multiline string
+console.log(mydailyroutine);
---
+++ index.ts
@@ -0,0 +1,7 @@
+//Q NO 50 using template litrals to define a multiple lines
+let mydailyroutine = `my daily routine is :
+1.wake up early and go to job.
+2. start coding .
+3.make some videos for my yt channel.`;
+// now console multiline string
+console.log(mydailyroutine);
